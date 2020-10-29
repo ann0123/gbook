bbbf0e33a31603ad17fad51fa9f87fbba79f443a
Ann Tung <10935143@dcsh.tp.edu.tw>
2020-10-29 10:30:07 +0800

    定義資料模型

--- poll/default/models.py
+++ poll/default/models.py
@@ -6,7 +6,12 @@ class Poll(models.Model):
     # 投票主題文字，至多 200 字
     subject = models.CharField(max_length=200)
     # 投票建立日期，在建立時若未指定，則自動填入建立時的時間
+    description = models.TextField()
     date_created = models.DateField(auto_now_add=True)
+
+    def __str__(self):
+        return str(self.id) + ")" + self.subject
+    
 
 class Option(models.Model):
     # 此選項屬於哪一個投票
@@ -15,3 +20,6 @@ class Option(models.Model):
     title = models.CharField(max_length=200)
     # 此選項被投票數
     count = models.IntegerField(default=0)
+
+    def __str__(self):
+        return str(self.poll_id) + ": " + self.title
